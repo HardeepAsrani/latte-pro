d2692b4764a6abd333f459776af71fc760da9f21
Hardeep Asrani <me@hardeepasrani.com>
2015-12-08 20:42:53 +0530

    Adds section order to the theme.
    Fixes: https://github.com/HardeepAsrani/latte-pro/issues/2

--- template-home.php
+++ template-home.php
@@ -18,57 +18,119 @@
 	$latte_map_display = get_theme_mod('latte_map_display');
 	$latte_contact_display = get_theme_mod('latte_contact_display', 1);
 
-	if( isset($latte_intro_display) && $latte_intro_display != 1 ):
-		get_template_part( 'sections/intro' );
-	endif;
+	$section1 = get_theme_mod('section_intro_order','section_intro');
+	$section2 = get_theme_mod('section_about_order','section_about');
+	$section3 = get_theme_mod('section_social_order','section_social');
+	$section4 = get_theme_mod('section_services_order','section_services');
+	$section5 = get_theme_mod('section_separator_order','section_separator');
+	$section6 = get_theme_mod('section_skills_order','section_skills');
+	$section7 = get_theme_mod('section_portfolio_order','section_portfolio');
+	$section8 = get_theme_mod('section_subscribe_order','section_subscribe');
+	$section9 = get_theme_mod('section_pricing_order','section_pricing');
+	$section10 = get_theme_mod('section_testimonials_order','section_testimonials');
+	$section11 = get_theme_mod('section_blogposts_order','section_blogposts');
+	$section12 = get_theme_mod('section_map_order','section_map');
+	$section13 = get_theme_mod('section_contact_order','section_contact');
+	
+	$sections[0] = $section1;
+	$sections[1] = $section2;
+	$sections[2] = $section3;
+	$sections[3] = $section4;
+	$sections[4] = $section5;
+	$sections[5] = $section6;
+	$sections[6] = $section7;
+	$sections[7] = $section8;
+	$sections[8] = $section9;
+	$sections[9] = $section10;
+	$sections[10] = $section11;
+	$sections[11] = $section12;
+	$sections[12] = $section13;
 
-	if( isset($latte_about_display) && $latte_about_display != 1 ):
-		get_template_part( 'sections/about' );
-	endif;
+	for ($i = 0; $i < 13; $i++):
+		if( !empty($sections[$i]) ):
+			switch ( $sections[$i] ) :
 
-	if( isset($latte_social_display) && $latte_social_display != 1 ):
-		get_template_part( 'sections/social' );
-	endif;
+				case "section_intro":
+					if( isset($latte_intro_display) && $latte_intro_display != 1 ):
+						get_template_part( 'sections/intro' );
+					endif;
+				break;
 
-	if( isset($latte_services_display) && $latte_services_display != 1 ):
-		get_template_part( 'sections/services' );
-	endif;
+				case "section_about":
+					if( isset($latte_about_display) && $latte_about_display != 1 ):
+						get_template_part( 'sections/about' );
+					endif;
+				break;
 
-	if( isset($latte_separator_display) && $latte_separator_display != 1 ):
-		get_template_part( 'sections/separator' );
-	endif;
+				case "section_social":
+					if( isset($latte_social_display) && $latte_social_display != 1 ):
+						get_template_part( 'sections/social' );
+					endif;
+				break;
 
-	if( isset($latte_skills_display) && $latte_skills_display != 1 ):
-		get_template_part( 'sections/skills' );
-	endif;
+				case "section_services":
+					if( isset($latte_services_display) && $latte_services_display != 1 ):
+						get_template_part( 'sections/services' );
+					endif;
+				break;
 
-	if( isset($latte_portfolio_display) && $latte_portfolio_display != 1 ):
-		get_template_part( 'sections/portfolio' );
-	endif;
+				case "section_separator":
+					if( isset($latte_separator_display) && $latte_separator_display != 1 ):
+						get_template_part( 'sections/separator' );
+					endif;
+				break;
 
-	if( isset($latte_subscribe_display) && $latte_subscribe_display != 1 ):
-		get_template_part( 'sections/subscribe' );
-	endif;
+				case "section_skills":
+						if( isset($latte_skills_display) && $latte_skills_display != 1 ):
+							get_template_part( 'sections/skills' );
+						endif;
+				break;
 
-	if( isset($latte_pricing_display) && $latte_pricing_display != 1 ):
-		get_template_part( 'sections/pricing' );
-	endif;
+				case "section_portfolio":
+					if( isset($latte_portfolio_display) && $latte_portfolio_display != 1 ):
+						get_template_part( 'sections/portfolio' );
+					endif;
+				break;
 
-	if( isset($latte_testimonials_display) && $latte_testimonials_display != 1 ):
-		get_template_part( 'sections/testimonials' );
-	endif;
+				case "section_portfolio":
+					if( isset($latte_subscribe_display) && $latte_subscribe_display != 1 ):
+						get_template_part( 'sections/subscribe' );
+					endif;
+				break;
 
-	if( isset($latte_blogposts_display) && $latte_blogposts_display != 1 ):
-		get_template_part( 'sections/blogposts' );
-	endif;
+				case "section_portfolio":
+					if( isset($latte_pricing_display) && $latte_pricing_display != 1 ):
+						get_template_part( 'sections/pricing' );
+					endif;
+				break;
 
-	if( isset($latte_map_display) && $latte_map_display != 1 ):
-		get_template_part( 'sections/map' );
-	endif;
+				case "section_testimonials":
+					if( isset($latte_testimonials_display) && $latte_testimonials_display != 1 ):
+						get_template_part( 'sections/testimonials' );
+					endif;
+				break;
 
-	if( isset($latte_contact_display) && $latte_contact_display != 1 ):
-		get_template_part( 'sections/contact' );
-	endif;
+				case "section_blogposts":
+					if( isset($latte_blogposts_display) && $latte_blogposts_display != 1 ):
+						get_template_part( 'sections/blogposts' );
+					endif;
+				break;
+
+				case "section_map":
+					if( isset($latte_map_display) && $latte_map_display != 1 ):
+						get_template_part( 'sections/map' );
+					endif;
+				break;
+
+				case "section_contact":
+					if( isset($latte_contact_display) && $latte_contact_display != 1 ):
+						get_template_part( 'sections/contact' );
+					endif;
+				break;
+
+			endswitch;
+		endif;
+	endfor;
 
 	get_footer();
 
